9ee23ecddcc476ff1c24dde420da3918bea88b59
bulutbilisimbussiness <bulutbilisimbussiness@gmail.com>
2023-08-29 23:24:05 +0300

    next

---
+++ app/(private)/movies/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const metadata = {
+  title: "Movies",
+};
+const Movies = () => {
+  return <div></div>;
+};
+
+export default Movies;
---
+++ app/(private)/profile/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const metadata = {
+  title: "Profile",
+};
+const Profile = () => {
+  return <div></div>;
+};
+
+export default Profile;
